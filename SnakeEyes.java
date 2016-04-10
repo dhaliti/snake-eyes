4be319d1b35ef3192474a1b4e7491efd1282fd80
dhaliti <xdreni@gmail.com>
2016-04-10 09:15:27 +0200

    Update SnakeEyes.java

--- SnakeEyes.java
+++ SnakeEyes.java
@@ -6,25 +6,25 @@ import java.util.Random;
  * @author dwen
  */
  
-public class SnakeEyes 
-{
+public class SnakeEyes{
     
-public static void main(String args[])
-   {
+public static void main(String args[]){
     Random rand = new Random();
     
     int dice1;
     int dice2;
     int count = 0;
-    
-    
+   
     while(true){
-        if((dice1 = rand.nextInt(6)+1) == 1 && (dice1 = rand.nextInt(6)+1) == 1){
-            System.out.printf("The dics rolled %d times till \"Snake eyes\" happend\n", count);
-            break;
-        }else count +=1;
+        dice1 = rand.nextInt(6)+1;
+        dice2 = rand.nextInt(6)+1;
+        System.out.printf("Dice one rolled |%d|, dice two rolled |%d|.\n", dice1,dice2);
+        count +=1;
         
-       
+        if(dice1 == 1 && dice2 == 1){
+            System.out.printf("It rolled \"Snake eyes\" after %d times of rolling.\n", count);
+            break;
+          }
     }
     
     
